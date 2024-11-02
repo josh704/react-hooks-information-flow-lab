964683a8ce56bdd9cc0f3ddbe199a2ae4347f15e
josh704 <joshuangetich02@gmail.com>
2024-11-02 13:39:11 +0300

    flow-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
-
+import Header from "./Header";
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   function onDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
-
+  const onCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
+    <div className={`App ${isDarkMode ? "dark" : "light"}`}>
+      <Header onDarkModeClick={onDarkModeClick} />
+      <ShoppingList 
+        items={itemData} 
+        selectedCategory={selectedCategory} 
+        onCategoryChange={onCategoryChange} 
+      />
+      </div>
   );
 }
 
